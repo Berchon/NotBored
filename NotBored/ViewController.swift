daddad610b6662d0ebe4dcdbd54cf0e90e695b49
Luciano Berchon <berchon@gmail.com>
2023-09-27 23:29:54 -0300

    Create project

---
+++ NotBored/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotBored
+//
+//  Created by Luciano Da Silva Berchon on 27/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
